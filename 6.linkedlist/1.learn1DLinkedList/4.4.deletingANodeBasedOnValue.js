c5837264c23ad6b232b8fc07345edad9d0b8e06b
Kalidass <kalidass.k021@gmail.com>
2024-09-13 21:44:09 +0530

    deleting a node based on value

---
+++ 6.linkedlist/1.learn1DLinkedList/4.4.deletingANodeBasedOnValue.js
@@ -0,0 +1,96 @@
+// deleting a node based on value
+// in sheet it's a 3rd problem
+// convert array into linked list
+let arr = [1, 8, 9, 3];
+// define a node class to represent each element in the linked list
+class Node {
+  constructor(data1) {
+    this.data = data1; // store the data
+    this.next = null; // pointer to the next node, initially set to null
+  }
+}
+
+// function to convert array to linked list
+function arrToLinkedList(arr) {
+  // check if arr is empty
+  // if arr.length === false
+  if (!arr.length) {
+    return null;
+  }
+
+  // create a head node with first element of the array
+  let head = new Node(arr[0]);
+  // initialize mover pointer to traverse the linked list
+  // initially set to head
+  let mover = head;
+  // loop through the array starting from the 2nd element
+  for (let i = 1; i < arr.length; i++) {
+    // create a new node with each element in the array
+    let temp = new Node(arr[i]);
+    // link the current node with new node
+    mover.next = temp;
+    // move the pointer to the next node
+    mover = mover.next;
+    // or
+    // mover = temp;
+  }
+
+  // return the head of the linked list
+  return head;
+}
+
+// function to traverse and print the linked list
+function printLinkedList(head) {
+  let temp = head;
+  // while head !== null
+  while (temp) {
+    // print the data of the current node
+    // console.log(temp.data);
+    process.stdout.write(String(temp.data) + ' ');
+    // move to the next node
+    temp = temp.next;
+  }
+}
+
+// delete node from the linked list based on value
+function removeElement(head, val) {
+  // if the list is empty, return null
+  if (!head) {
+    return null;
+  }
+
+  // if element is head.data, remove head
+  // move the head to next
+  if (head.data === val) {
+    head = head.next;
+    return head;
+    // or
+    // return head.next;
+  }
+
+  // if k is inbetween any element
+  let temp = head;
+  let prev = null;
+  // while temp !== null
+  while (temp) {
+    // condition
+    if (temp.data === val) {
+      prev.next = prev.next.next;
+    }
+
+    // remember temp as the previous while moving to next
+    prev = temp;
+    // traversal
+    temp = temp.next;
+  }
+
+  // above logic will delete the tail of the linked list if k = 4
+  // returning the head of the linked list
+  return head;
+}
+
+let head = arrToLinkedList(arr);
+// head of the modified linked list
+head = removeElement(head, 8);
+// print the linked list
+printLinkedList(head);
