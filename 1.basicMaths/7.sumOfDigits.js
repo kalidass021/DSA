5faf43cdff6f045c3f5091e3f56a21ea8ca3c6a1
Kalidass <kalidass.k021@gmail.com>
2025-02-27 22:46:54 +0530

    add recursive solution

--- 1.basicMaths/7.sumOfDigits.js
+++ 1.basicMaths/7.sumOfDigits.js
@@ -10,7 +10,7 @@ function sumOfDigits(num) {
     }
 
     // edge case for single digit number
-    if (num>1 && num<=9) {
+    if (num>=1 && num<=9) {
         return num;
     }
 
@@ -28,4 +28,25 @@ function sumOfDigits(num) {
     return sum;
 }
 
-console.log(sumOfDigits(12345));
+console.log(sumOfDigits(12345)); // 15
+
+// recursive solution
+function sumOfDigitsRec(num) {
+    if (typeof num !== 'number' || !Number.isInteger(num)) {
+        return `${num} is not a positive integer`;
+    }
+
+    if (num === 0) {
+        return 0;
+    }
+
+    if (num>=1 && num<=9) {
+        return num;
+    }
+
+    // recursive case
+    // get the last digit and add it to the sum of the remaining digits
+    return num % 10 + sumOfDigitsRec(Math.floor(num/10));
+}
+
+console.log(sumOfDigitsRec(12345)); // 15
