497c0fbb791dccbeb17867fee1c23c52168db358
Kalidass <kalidass.k021@gmail.com>
2025-03-03 23:00:06 +0530

    stack essentials initial commit

---
+++ 9.stackAndQueues/1.stacks/0.essential/0.basics.js
@@ -0,0 +1,14 @@
+// Last In First Out (LIFO)
+// or
+// First In Last Out
+
+
+/* 
+    functions that stack can perform
+        1. push(element) ==> Allows us to push something inside the stack
+        2. pop() ==> last element come out of the stack
+        3. peek() ==> peeks allows us to see which element currently on the top
+        4. isEmpty() ==> if the stack is empty of not, returns boolean
+        5. size() ==> howmany elements currently inside of the stack
+*/
+
