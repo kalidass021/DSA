f73e5ae8fa7e7e7eec4ed2a429238c33a55cef40
Kalidass <kalidass.k021@gmail.com>
2025-03-04 21:03:40 +0530

    two sum optimal solution

--- 3.arrays/0.essential/4.twoSum.js
+++ 3.arrays/0.essential/4.twoSum.js
@@ -23,4 +23,27 @@ function bruteforce(arr, target) {
   return [];
 }
 
-console.log(bruteforce(arr, target));
+console.log(bruteforce(arr, target)); // [ 6, 8 ]
+
+// optimal solution using hash map
+
+function optimal(arr, target) {
+    // create a Map to store the elements of the array
+    let myMap = new Map();
+
+    for (let val of arr) {
+        // calculate the value needed to reach the target
+        const moreNeeded = target - val;
+        // check if the needed value is already in the Map
+        if (myMap.has(moreNeeded)) {
+            // if found, return the pair of numbers that add up to the target value
+            return [val, moreNeeded];
+        }
+        // if not found, add the current element to the Map
+        myMap.set(val);
+    }
+    // if no pair is found, return the empty array
+    return [];
+}
+
+console.log(optimal(arr, target)); // [ 8, 6 ]
