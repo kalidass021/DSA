a20060aeccb3abd2634e91c712f7602271f95249
Kalidass <kalidass.k021@gmail.com>
2025-03-05 13:35:18 +0530

    queue basics

---
+++ 9.stackAndQueues/2.queues/0.essential/0.basics.js
@@ -0,0 +1,18 @@
+// First In First Out (FIFO)
+// or
+// Last In Last Out
+
+/* 
+    A queue is a linear data structure that follows a particular order
+    in which the operations are performed. The order is First In First Out (FIFO) or
+    Last In Last Out (LILO)
+*/
+
+/* 
+    functions that queue can perfrom
+        1. enqueue(element) ==> Responsible for adding the element inside the queue
+        2. dequeue() ==> Responsible for taking out the element from the queue
+        3. front()/ peek() ==> Gives the element at the front of our queue
+        4. isEmpty() ==> if the queue is empty or not, returns boolean
+        5. size() ==> howmany elements currently inside the queue
+*/
