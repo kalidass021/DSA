4858fffb96e2c0bc2b078b2d010e72b7f1bb0f4c
Kalidass <kalidass.k021@gmail.com>
2025-03-20 12:31:07 +0530

    print fibonacci series in reverse

--- 1.basicMaths/9.fibonacciSeries.js
+++ 1.basicMaths/9.fibonacciSeries.js
@@ -23,3 +23,33 @@ function fibonacci(num) {
 }
 
 console.log(fibonacci(5));
+
+// print fibonacci series in reverse order
+
+// we can directly reverse the above result using inbuilt reverse function
+// or we can reverse using for (let i=arr.length - 1; i>=0; i--)
+// but these will increase the time complexity to O(n^2)
+
+function fibonacciInReverse(num) {
+    if (typeof num !== 'number' || !Number.isInteger(num) || num<0) {
+        return `${num} is not a positive integer`;
+    }
+
+    if (num === 0) {
+        return [0];
+    }
+
+    if (num === 1) {
+        return [1, 0];
+    }
+
+    let fibSeriesInReverse = [1, 0]; // initially
+
+    for (let i=2; i<num; i++) {
+        fibSeriesInReverse.unshift(fibSeriesInReverse[1] + fibSeriesInReverse[0]);
+    }
+
+    return fibSeriesInReverse;
+}
+
+console.log(fibonacciInReverse(10));
