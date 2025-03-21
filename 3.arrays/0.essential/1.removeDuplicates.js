c5e578c32df90c8278cb81aa13b383e62625fd06
Kalidass <kalidass.k021@gmail.com>
2025-03-21 18:51:22 +0530

    remove duplicates using hash map

--- 3.arrays/0.essential/1.removeDuplicates.js
+++ 3.arrays/0.essential/1.removeDuplicates.js
@@ -3,33 +3,52 @@
 let arr = [1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 5];
 
 function removeDuplicates(arr) {
-    let mySet = new Set();
-    let uniqueArr = []; // to store the result
+  let mySet = new Set();
+  let uniqueArr = []; // to store the result
 
-    // loop through the array and put all items into set
-    // set automatically remove the duplicates
-    for (let val of arr) {
-        mySet.add(val);
-    }
+  // loop through the array and put all items into set
+  // set automatically remove the duplicates
+  for (let val of arr) {
+    mySet.add(val);
+  }
 
-    // loop through the set and put unique elements into an new array
-    for (let val of mySet) {
-        uniqueArr.push(val);
-    }
+  // loop through the set and put unique elements into an new array
+  for (let val of mySet) {
+    uniqueArr.push(val);
+  }
 
-    return uniqueArr;
+  return uniqueArr;
 }
 
 console.log(removeDuplicates(arr));
 
+// using hash map
+
+function removeDuplicatesHash(arr) {
+  let myMap = new Map();
+  let uniqueArr = []; // to store the result
+
+  for (let val of arr) {
+    // if the val already not present in the map
+    if (!myMap.has(val)) {
+      myMap.set(val, true); // add the val to the map
+      uniqueArr.push(val); // push the val to the uniqueArr
+    }
+  }
+
+  return uniqueArr;
+}
+
+console.log(removeDuplicatesHash(arr));
+
 // simple solution using language advantage
 
 function removeDuplicates2(arr) {
-    let mySet = new Set(arr);
-    // spread the set inside the array
-    let uniqueArr = [...mySet];
+  let mySet = new Set(arr);
+  // spread the set inside the array
+  let uniqueArr = [...mySet];
 
-    return uniqueArr;
+  return uniqueArr;
 }
 
 console.log(removeDuplicates2(arr));
