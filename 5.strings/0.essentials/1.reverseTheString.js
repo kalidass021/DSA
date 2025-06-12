dedb606026d791701d460bdff7cb4729ca0790a7
Kalidass <kalidass.k021@gmail.com>
2025-06-12 22:52:51 +0530

    reverse the string using reduce

--- 5.strings/0.essentials/1.reverseTheString.js
+++ 5.strings/0.essentials/1.reverseTheString.js
@@ -3,14 +3,14 @@
 let str = 'abcdefg';
 
 function reverseTheString(str) {
-    let reversed = ''; // initially
+  let reversed = ''; // initially
 
-    for (let i=str.length-1; i>=0; i--) {
-        // append the current character
-        reversed += str[i]; 
-    }
+  for (let i = str.length - 1; i >= 0; i--) {
+    // append the current character
+    reversed += str[i];
+  }
 
-    return reversed;
+  return reversed;
 }
 
 console.log(reverseTheString(str)); // gfedcba
@@ -18,14 +18,28 @@ console.log(reverseTheString(str)); // gfedcba
 // simple solution using language advantage
 
 function reverseTheString2(str) {
-    // split string into an array
-    let arr = [...str];
-    // reverse the array
-    arr.reverse();
-    // join the array into string
-    let reversed = arr.join('');
+  // split string into an array
+  let arr = [...str];
+  // reverse the array
+  arr.reverse();
+  // join the array into string
+  let reversed = arr.join('');
 
-    return reversed;
+  return reversed;
 }
 
 console.log(reverseTheString2(str)); // gfedcba
+
+function reverseTheString3(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseTheString3('hello')); // olleh
+
+// using reduce
+
+function reverseTheStringReduce(str) {
+  return str.split('').reduce((reversed, currChar) => currChar + reversed, '');
+}
+
+console.log(reverseTheStringReduce('hello')); // olleh
