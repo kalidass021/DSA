e90e2d9cfc79e68a551780b3693406808fb9e899
Kalidass <kalidass.k021@gmail.com>
2025-06-16 23:34:39 +0530

    use .at to get the specific element of the item

--- 1.basicMaths/9.fibonacciSeries.js
+++ 1.basicMaths/9.fibonacciSeries.js
@@ -1,26 +1,26 @@
 // fibonacci series
 
 function fibonacci(num) {
-    if (typeof num !== 'number' || !Number.isInteger(num) || num<0) {
-        return `${num} is not a positive integer`;
-    }
-    
-    if (num === 0) {
-        return [0];
-    }
+  if (typeof num !== 'number' || !Number.isInteger(num) || num < 0) {
+    return `${num} is not a positive integer`;
+  }
 
-    if (num === 1) {
-        return [0, 1];
-    }
+  if (num === 0) {
+    return [0];
+  }
 
-    let fibSeries = [0, 1];
+  if (num === 1) {
+    return [0, 1];
+  }
 
-    for (let i=2; i<num; i++) {
-        // fibSeries.push(fibSeries[i-2] + fibSeries[i-1]);
-        fibSeries.push(fibSeries.at(-1) + fibSeries.at(-2));
-    }
+  let fibSeries = [0, 1];
 
-    return fibSeries;
+  for (let i = 2; i < num; i++) {
+    // fibSeries.push(fibSeries[i-2] + fibSeries[i-1]);
+    fibSeries.push(fibSeries.at(-1) + fibSeries.at(-2));
+  }
+
+  return fibSeries;
 }
 
 console.log(fibonacci(5));
@@ -32,25 +32,28 @@ console.log(fibonacci(5));
 // but these will increase the time complexity to O(2n)
 
 function fibonacciInReverse(num) {
-    if (typeof num !== 'number' || !Number.isInteger(num) || num<0) {
-        return `${num} is not a positive integer`;
-    }
+  if (typeof num !== 'number' || !Number.isInteger(num) || num < 0) {
+    return `${num} is not a positive integer`;
+  }
 
-    if (num === 0) {
-        return [0];
-    }
+  if (num === 0) {
+    return [0];
+  }
 
-    if (num === 1) {
-        return [1, 0];
-    }
+  if (num === 1) {
+    return [1, 0];
+  }
 
-    let fibSeriesInReverse = [1, 0]; // initially
+  let fibSeriesInReverse = [1, 0]; // initially
 
-    for (let i=2; i<num; i++) {
-        fibSeriesInReverse.unshift(fibSeriesInReverse[1] + fibSeriesInReverse[0]);
-    }
+  for (let i = 2; i < num; i++) {
+    // fibSeriesInReverse.unshift(fibSeriesInReverse[1] + fibSeriesInReverse[0]);
+    fibSeriesInReverse.unshift(
+      fibSeriesInReverse.at(1) + fibSeriesInReverse.at(0)
+    );
+  }
 
-    return fibSeriesInReverse;
+  return fibSeriesInReverse;
 }
 
 console.log(fibonacciInReverse(10));
