fa2addd809684604bc5552227ba9680a46b3aacb
Kalidass <kalidass.k021@gmail.com>
2025-07-14 23:57:42 +0530

    simple solution using Array.includes method

--- 3.arrays/0.essential/9.differenceBetweenArrays.js
+++ 3.arrays/0.essential/9.differenceBetweenArrays.js
@@ -1,10 +1,18 @@
 function differenceBetweenArrays(arr1, arr2) {
+    return arr1.filter(item => !arr2.includes(item));
+}
+
+console.log(differenceBetweenArrays([1, 2, 3, 4], [2, 4]));
+console.log(differenceBetweenArrays(['a', 'b', 'c'], ['b']));
+
+// using set
+function differenceBetweenArraysSet(arr1, arr2) {
     const set2 = new Set(arr2);
 
     return arr1.filter(item => !set2.has(item));
 }
 
 
-console.log(differenceBetweenArrays([1, 2, 3, 4], [2, 4])); // [1, 3]
+console.log(differenceBetweenArraysSet([1, 2, 3, 4], [2, 4])); // [1, 3]
 
-console.log(differenceBetweenArrays(['a', 'b', 'c'], ['b'])); // ['a', 'c']
+console.log(differenceBetweenArraysSet(['a', 'b', 'c'], ['b'])); // ['a', 'c']
