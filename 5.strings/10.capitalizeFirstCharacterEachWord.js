61b0c74991f2fb847ee6eb76797ee37f31ffd17d
Kalidass <kalidass.k021@gmail.com>
2025-08-02 23:56:08 +0530

    capitalize the first character of the each word in the string

---
+++ 5.strings/10.capitalizeFirstCharacterEachWord.js
@@ -0,0 +1,15 @@
+// capitalize the first character of the each word in the given string
+// my name is kalidass ==> My Name Is Kalidass
+
+function capitalizeFirstCharacterOfEachWord(str) {
+    let words = str.split(' ');
+
+    let res;
+
+    for (let word of words) {
+
+    }
+    return res;
+}
+
+console.log(capitalizeFirstCharacterOfEachWord('my name is kalidass'));
