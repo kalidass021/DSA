e248cdb839d6b3bcc159b4ace12053a3fc2cb6bb
Kalidass <kalidass.k021@gmail.com>
2025-08-10 15:40:18 +0530

    flatten the nested arrays

---
+++ 3.arrays/0.essential/12.flattenTheNestedArrays.js
@@ -0,0 +1,30 @@
+// flatten the nested arrays
+
+let nestedArr = [1, [2, [3, [4, [5]]]]];
+function flatArr(nestedArr) {
+    let result = nestedArr.flat(Infinity);
+
+    return result;
+}
+
+console.log(flatArr(nestedArr)); // [ 1, 2, 3, 4, 5 ]
+
+// using recursion
+
+function flattenRecursive(nestedArr) {
+    let result = []; // initially
+
+    for (let item of nestedArr) {
+        if (Array.isArray(item)) {
+            let flattenedItem = flattenRecursive(item);
+            result = [...result, ...flattenedItem]; // spread the flattened sub array
+        } else {
+            // retult = [...result, item];
+            result.push(item);
+        }
+    }
+
+    return result;
+}
+
+console.log(flattenRecursive(nestedArr)); //
