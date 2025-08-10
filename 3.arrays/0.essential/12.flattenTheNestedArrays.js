875b19d14b35de75ea56991edea64a1b973cd056
Kalidass <kalidass.k021@gmail.com>
2025-08-10 15:46:36 +0530

    flatten the nested arrays using recursion and reduce

--- 3.arrays/0.essential/12.flattenTheNestedArrays.js
+++ 3.arrays/0.essential/12.flattenTheNestedArrays.js
@@ -2,9 +2,9 @@
 
 let nestedArr = [1, [2, [3, [4, [5]]]]];
 function flatArr(nestedArr) {
-    let result = nestedArr.flat(Infinity);
+  let result = nestedArr.flat(Infinity);
 
-    return result;
+  return result;
 }
 
 console.log(flatArr(nestedArr)); // [ 1, 2, 3, 4, 5 ]
@@ -12,19 +12,31 @@ console.log(flatArr(nestedArr)); // [ 1, 2, 3, 4, 5 ]
 // using recursion
 
 function flattenRecursive(nestedArr) {
-    let result = []; // initially
+  let result = []; // initially
 
-    for (let item of nestedArr) {
-        if (Array.isArray(item)) {
-            let flattenedItem = flattenRecursive(item);
-            result = [...result, ...flattenedItem]; // spread the flattened sub array
-        } else {
-            // retult = [...result, item];
-            result.push(item);
-        }
+  for (let item of nestedArr) {
+    if (Array.isArray(item)) {
+      let flattenedItem = flattenRecursive(item);
+      result = [...result, ...flattenedItem]; // spread the flattened sub array
+    } else {
+      // retult = [...result, item];
+      result.push(item);
     }
+  }
 
-    return result;
+  return result;
 }
 
-console.log(flattenRecursive(nestedArr)); //
+console.log(flattenRecursive(nestedArr)); // [ 1, 2, 3, 4, 5 ]
+
+function flattenRecursiveReduce(nestedArr) {
+  let result = nestedArr.reduce((acc, curr) => {
+    return Array.isArray(curr)
+      ? [...acc, ...flattenRecursiveReduce(curr)]
+      : [...acc, curr];
+  }, []);
+
+  return result;
+}
+
+console.log(flattenRecursiveReduce(nestedArr));
