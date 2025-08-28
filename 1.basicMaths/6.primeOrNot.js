ec61c1461e3686c2f97366decad63ccc1ad7c805
Kalidass <kalidass.k021@gmail.com>
2025-08-28 14:55:54 +0530

    prime number optimal using Math.sqrt

--- 1.basicMaths/6.primeOrNot.js
+++ 1.basicMaths/6.primeOrNot.js
@@ -23,3 +23,29 @@ function isPrime(num) {
 }
 
 console.log(isPrime(2));
+
+
+function isPrimeOptimal(num) {
+    if (typeof num !== 'number') {
+        return `${num} is not a valid number`;
+    }
+
+    if (num < 0) {
+        return `${num} is not a positive integer`;
+    }
+
+    if (num <= 1) return false;
+
+    /* 
+        * We use Math.sqrt(num) to check fewer numbers,
+        * because if a number has a factor bigger than it's squre root,
+        * it also has a smaller one we already checked.
+    */
+    for (let i=2; i<=Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+}
+
+console.log(isPrimeOptimal(4));
