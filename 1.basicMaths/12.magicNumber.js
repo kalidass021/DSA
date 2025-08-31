412a473cb88f40e07093ac7b7784bc2aa4932798
Kalidass <kalidass.k021@gmail.com>
2025-08-31 19:14:45 +0530

    magic number traditional approach

--- 1.basicMaths/12.magicNumber.js
+++ 1.basicMaths/12.magicNumber.js
@@ -1,8 +1,35 @@
-function isMagicNumber(num) {
-    let sum = num; // initially
+/* 
+    * A magic number is a number whose sum of digits, when repeatedly added,
+    * untill a single digit is left, results in 1
+    * Take 1234
+    * 1 + 2 + 3 + 4
+    * 1 + 0 = 1, 1234 is a magic number
+    * 
+    * Another shortcut
+    * If a number modulo 9 equals 1, it's a magic number
+*/
 
-    // keep summing digits until a single digit is obtained
+function isMagicNumber(num) {
+    let temp = num;
+    // Keep reducing the number until it's a single digit
+    while (temp >= 10) {
+        let sum = 0; // initially
+        // Extract and sum each digit of the current number
+        while (temp > 0) {
+            let lastDigit = temp % 10;
+            sum += lastDigit;
+            // drop the last digit
+            temp = Math.floor(temp / 10);
+        }
+    // replace temp with the sum of digits for the next iteration
+    temp = sum;
+    }
+
+    return temp === 1;
 }
+
+
+console.log('1234', isMagicNumber(1234));
 
 
 // Todo: 2 approaches needs to be added 1. normal while loop 2. using recursion
