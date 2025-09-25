59e1188a5125a0ad8aaaf6a91d718b8d862d8a7a
Kalidass <kalidass.k021@gmail.com>
2025-09-25 23:19:41 +0530

    sum of elements reduce

--- 3.arrays/0.essential/17.sumOfElements.js
+++ 3.arrays/0.essential/17.sumOfElements.js
@@ -1,13 +1,17 @@
 // sum of elements in the array
 
 function sumOfElements(arr) {
-    let sum = 0; // initially
+  let sum = 0; // initially
 
-    for (let num of arr) {
-        sum += num;
-    }
-    // return the sum
-    return sum;
+  for (let num of arr) {
+    sum += num;
+  }
+  // return the sum
+  return sum;
 }
 
-console.log(sumOfElements([1, 2, 3, 4, 5]));
+console.log(sumOfElements([1, 2, 3, 4, 5])); // 15
+
+const sumOfElementsReduce = (arr) => arr.reduce((acc, curr) => (acc += curr));
+
+console.log(sumOfElementsReduce([1, 2, 3, 4, 5]));
